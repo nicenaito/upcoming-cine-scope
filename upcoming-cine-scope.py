53ccf24712d9c758e193a2d194fcc2e501c8e655
fukunaga <nicenaito@gmail.com>
2023-07-26 09:59:11 +0900

    映画のあらすじを取得する処理を追加した。一部、変数名を修正した。

--- upcoming-cine-scope.py
+++ upcoming-cine-scope.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import datetime
 
 # Get the HTML of the page from cinema-today
 def get_html(url):
@@ -20,10 +21,28 @@ def parse_movie_url(html):
 # Parse the HTML and get the movie detail
 def parse_movie_detail(html):
     soup = BeautifulSoup(html, 'html.parser')
-    tutle = soup.find("h1", itemprop="name")
-    print(tutle.contents[0])
-    pub_date = soup.find("span", class_="published")
-    print(pub_date.contents[1])
+    
+    # Get movie title
+    title_tag = soup.find("h1", itemprop="name")
+    title = title_tag.contents[0]
+    print(title)
+    
+    # Get movie release date
+    pub_date_tag = soup.find("span", class_="published")
+    # print(pub_date.contents[1])
+    s_format = '%Y年%m月%d日'
+    pub_date_str = pub_date_tag.contents[1].strip().replace('公開', '')
+    pub_date = datetime.datetime.strptime(pub_date_str, s_format)
+    print(pub_date)
+    
+    # Get description
+    description_tag = soup.find("p", itemprop="description")
+    description = description_tag.contents[0]
+    print(description)
+    
+    # TODO: #1 Implement getting movie summary
+    # summary_tag = soup.find_all("section","")
+    # print(summary_tag[1])
 
 if __name__ == "__main__":
     url = 'https://www.cinematoday.jp/movie/release/'
