15eaec9dfe79cf74d5e62fe26b61305415d3656f
fukunaga <nicenaito@gmail.com>
2023-07-27 09:25:26 +0900

    movie_idをURLから取得するようにした。取得した各値を辞書とリストに格納するよう実装した。

--- upcoming-cine-scope.py
+++ upcoming-cine-scope.py
@@ -19,13 +19,21 @@ def parse_movie_url(html):
     return movie_url_list
 
 # Parse the HTML and get the movie detail
-def parse_movie_detail(html):
+def parse_movie_detail(html, movie_url):
     soup = BeautifulSoup(html, 'html.parser')
+    
+    movie_dict = {}
+    
+    # Get the movie id from URL
+    movie_id = movie_url.split('/')[-1]
+    print(movie_id)
+    movie_dict['movie_id'] = movie_id
     
     # Get movie title
     title_tag = soup.find("h1", itemprop="name")
     title = title_tag.contents[0]
     print(title)
+    movie_dict['title'] = title
     
     # Get movie release date
     pub_date_tag = soup.find("span", class_="published")
@@ -34,20 +42,31 @@ def parse_movie_detail(html):
     pub_date_str = pub_date_tag.contents[1].strip().replace('公開', '')
     pub_date = datetime.datetime.strptime(pub_date_str, s_format)
     print(pub_date)
+    movie_dict['pub_date'] = pub_date
     
     # Get description
     description_tag = soup.find("p", itemprop="description")
     description = description_tag.contents[0]
     print(description)
+    movie_dict['description'] = description
     
     # TODO: #1 Implement getting movie summary
     # summary_tag = soup.find_all("section","")
     # print(summary_tag[1])
+    
+    print(movie_dict)
+    
+    return movie_dict
 
 if __name__ == "__main__":
     url = 'https://www.cinematoday.jp/movie/release/'
     html = get_html(url)
     movie_url_list = parse_movie_url(html)
-    for movie in movie_url_list:
-        html = get_html(movie)
-        parse_movie_detail(html)
+    
+    movie_list = []
+    
+    for movie_url in movie_url_list:
+        html = get_html(movie_url)
+        movie_list.append(parse_movie_detail(html, movie_url))
+        
+    print(movie_list)
